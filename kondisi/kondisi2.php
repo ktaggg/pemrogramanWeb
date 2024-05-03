03ec4d49bf6b3daa64311c76b8e338376c187a49
ktaggg <ketutagung3690@gmail.com>
2024-05-03 22:55:41 +0800

    first commit

---
+++ kondisi/kondisi2.php
@@ -0,0 +1,11 @@
+<?php
+
+$username = "Ahmad";
+
+    if ($username == "Ahmad") {
+        echo "Anda Berhasil Login!, Selamat datang 
+$username";
+    } else {
+        echo "Username salah, silahkan coba lagi!";
+    }
+?>
---
+++ praktikum/prak1.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h1>INSTITUT BISNIS DAN TEKNOLOGI INDONESIA</h1>";
+    echo "Program PHP Pertamaku<Br>";
+?>
